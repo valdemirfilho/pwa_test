2f55192ce318f65c686a44c0fad0c5e60ecaee09
Valdemir Filho <filhux@gmail.com>
2020-10-20 17:51:33 -0300

    service worker fetch

--- sw.js
+++ sw.js
@@ -5,3 +5,11 @@ self.addEventListener("install", e => {
     })
   );
 });
+
+self.addEventListener("fetch" , e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);    
+    })
+  )
+})
