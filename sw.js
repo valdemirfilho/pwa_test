05854909fef0d99023372852510b9504baa22c19
Valdemir Filho <filhux@gmail.com>
2020-10-20 17:40:24 -0300

    service worker v4

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 self.addEventListener("install", e => {
-e.waitUntil(
-  caches.open("static").then(cache => {
-    return cache.addAll(["./", "styles.css", "img/logo192.png"]);
-  })
-); 
-
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["./", "./styles.css", "./img/logo192.png"]);
+    })
+  );
 });
