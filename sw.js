e478a05c268193804fada110757ede9f1e255f07
Valdemir Filho <filhux@gmail.com>
2020-10-22 14:25:41 -0300

    bugfix

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ self.addEventListener('activate', function(event){
 
 self.addEventListener('fetch', function(event){
   event.respondWith(
-    cache.match(event.request).then(function(response){
+    caches.match(event.request).then(function(response){
       return response || fetch(event.request);
     })
   );
